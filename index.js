2b429dd401635ef2d886721a05ef080370e298df
quockhanhoi <quockhanh.dalat13@gmail.com>
2026-04-08 02:50:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,75 @@
 const express = require("express");
-const axios = require("axios");
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
+
+puppeteer.use(StealthPlugin());
+
 const app = express();
 
+const USER_AGENT =
+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36";
+
 app.get("/", (req, res) => {
-res.send("facebook view api");
+res.send("Facebook Screenshot API");
 });
 
-app.get("/screenshot/:uid/:cookies", async (req, res) => {
+app.get("/screenshot/:uid", async (req, res) => {
 
 const uid = req.params.uid;
-const cookies = decodeURIComponent(req.params.cookies);
+const cookie = req.query.cookie;
+
+if (!cookie) {
+return res.json({
+error: true,
+message: "Missing cookie"
+});
+}
 
 try {
 
-const response = await axios.get(`https://www.facebook.com/${uid}`, {
-headers: {
-
-"User-Agent":
-"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36",
-
-"Accept":
-"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-
-"Accept-Language": "en-US,en;q=0.9",
-
-"Cookie": cookies
-
-}
+const browser = await puppeteer.launch({
+args: [
+"--no-sandbox",
+"--disable-setuid-sandbox"
+]
 });
 
-res.send(response.data);
+const page = await browser.newPage();
 
-} catch (err) {
+await page.setUserAgent(USER_AGENT);
 
-res.status(500).json({
+await page.setViewport({
+width: 1519,
+height: 900,
+deviceScaleFactor: 1,
+isMobile: false
+});
+
+await page.setExtraHTTPHeaders({
+cookie: cookie
+});
+
+await page.goto(`https://www.facebook.com/${uid}`, {
+waitUntil: "networkidle2",
+timeout: 60000
+});
+
+await new Promise(r => setTimeout(r, 4000));
+
+const image = await page.screenshot({
+type: "png"
+});
+
+await browser.close();
+
+res.set("Content-Type", "image/png");
+res.send(image);
+
+} catch (e) {
+
+res.json({
 error: true,
-message: err.message
+message: e.message
 });
 
 }
