5d531286eb12acbbb7ac1af7856023f5ffee0811
quockhanhoi <quockhanh.dalat13@gmail.com>
2026-04-08 02:44:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('API Facebook View');
+app.get("/", (req, res) => {
+res.send("facebook view api");
 });
 
-app.get('/screenshot/:uid/:cookies', async (req, res) => {
+app.get("/screenshot/:uid/:cookies", async (req, res) => {
 
-    const { uid, cookies } = req.params;
+const uid = req.params.uid;
+const cookies = decodeURIComponent(req.params.cookies);
 
-    try {
+try {
 
-        const response = await axios({
-            method: 'GET',
-            url: `https://www.facebook.com/${uid}`,
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                'Accept-Language': 'en-US,en;q=0.9',
-                'Sec-CH-UA': '"Google Chrome";v="120", "Chromium";v="120"',
-                'Sec-CH-UA-Mobile': '?0',
-                'Sec-CH-UA-Platform': '"Windows"',
-                'Upgrade-Insecure-Requests': '1',
-                'Viewport-Width': '1920',
-                'Cookie': cookies
-            }
-        });
+const response = await axios.get(`https://www.facebook.com/${uid}`, {
+headers: {
 
-        res.send(response.data);
+"User-Agent":
+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36",
 
-    } catch (err) {
+"Accept":
+"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
 
-        res.send({
-            error: true,
-            message: err.message
-        });
+"Accept-Language": "en-US,en;q=0.9",
 
-    }
+"Cookie": cookies
+
+}
+});
+
+res.send(response.data);
+
+} catch (err) {
+
+res.status(500).json({
+error: true,
+message: err.message
+});
+
+}
 
 });
 
-app.listen(process.env.PORT || 3000, () => console.log("Server running"));
+app.listen(process.env.PORT || 3000, () =>
+console.log("server running")
+);
